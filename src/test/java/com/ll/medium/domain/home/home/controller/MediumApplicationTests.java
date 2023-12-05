9b04a81706474390ee78c0f423b4b6a433d1a3be
silvercho <tjddms01212@gmail.com>
2023-12-05 23:30:36 +0900

    start

--- src/test/java/com/ll/medium/domain/home/home/controller/MediumApplicationTests.java
+++ src/test/java/com/ll/medium/domain/home/home/controller/MediumApplicationTests.java
@@ -4,10 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MediumApplicationTests {
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+class HomeControllerTest  {
 
+    @DisplayName("t1")
     @Test
-    void contextLoads() {
+    void t1() {
+
     }
 
 }
