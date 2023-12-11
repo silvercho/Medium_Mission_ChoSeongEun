9f508a0436ed28697faa637879eb03cd360707bd
silvercho <tjddms01212@gmail.com>
2023-12-11 17:12:33 +0900

    23

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -22,7 +22,16 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers("/**")
+                        authorizeRequests
+                                .requestMatchers("/gen/**")
+                                .permitAll()
+                                .requestMatchers("/resource/**")
+                                .permitAll()
+                                .requestMatchers("/h2-console/**")
+                                .permitAll()
+                                .requestMatchers("/adm/**")
+                                .hasRole("ADMIN")
+                                .anyRequest()
                                 .permitAll()
                 )
                 .headers(
