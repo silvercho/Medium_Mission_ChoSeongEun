586b5521b2735ea1a50a1eb0f70f0b74c4680f58
silvercho <tjddms01212@gmail.com>
2023-12-11 14:29:35 +0900

    failMsg

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -1,9 +1,10 @@
 package com.ll.medium.domain.member.member.entity;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,10 +12,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-@NoArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Builder
 @Getter
 @Setter
 public class Member {
@@ -28,8 +32,4 @@ public class Member {
     private String username;
     private String password;
 
-    public Member(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
--- src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -27,7 +27,10 @@ public class MemberService {
             return  RsData.of("400-2", "이미 존재하는 회원입니다.");
         }
 
-        Member member = new Member(username, passwordEncoder.encode(password));
+        Member member = Member.builder()
+                        .username(username)
+                                .password(passwordEncoder.encode(password))
+                                        .build();
 
         memberRepository.save(member);
 
